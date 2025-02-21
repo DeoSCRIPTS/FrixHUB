cd68232163ec54e2132c5b7b576345d99be6b856
DeoSCRIPTS <intelpower1189@gmail.com>
2025-02-21 14:57:44 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DeoSCRIPTS/FrixHUB/game.lua"))()
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "FrixHUB",
+Text = "Made by Singularitix (on discord hehe)", 
+
+Button1 = "Okay",
+Button2 = "Cancel",
+Duration = 10
+})
